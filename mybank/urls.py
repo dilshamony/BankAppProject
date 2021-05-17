19008aaa4abfa0e60a733a38f29430f7b3445873
Dilsha <dilsham998@gmail.com>
2021-05-17 18:56:06 +0530

    registration page

--- mybank/urls.py
+++ mybank/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from .views import Registration
 
 urlpatterns = [
-
+    path("register",Registration.as_view(),name="register")
 ]
--- mybank/views.py
+++ mybank/views.py
@@ -1,9 +1,21 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from  .forms import UserRegistrationForm
+from .models import CustomUser
+from django.views.generic import TemplateView
 
 # Create your views here.
+
 #REGISTRATION
 #using user model
 
-from django.contrib.auth.models import User
+class Registration(TemplateView):
+    form_class=UserRegistrationForm
+    template_name = "registration.html"
+    context={}
+    def get(self,request,*args,**kwargs):
+        self.context["form"]=self.form_class()
+        return render(request,self.template_name,self.context)
+
 
 #LOGIN
